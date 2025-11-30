3c2bd82734ad8b01999ff9ceb1c4c82f82e378da
Velmurugangsh <146573985+Velmurugangsh@users.noreply.github.com>
2025-11-30 20:10:22 +0530

    Fix import path for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
